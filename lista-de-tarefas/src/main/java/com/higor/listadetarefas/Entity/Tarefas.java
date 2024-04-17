d774dafbbb19b12c1ac26f85c59c875589eb8484
Higoralmeida702 <153138268+Higoralmeida702@users.noreply.github.com>
2024-04-16 23:04:11 -0300

    feat: Adiciona entidade Tarefas
    Nova classe para gerenciar tarefas na lista. Inclui identificação única, descrição, status de realização e prioridade. Pronta para mapeamento na tabela "listaTarefas" do banco de dados.

---
+++ lista-de-tarefas/src/main/java/com/higor/listadetarefas/Entity/Tarefas.java
@@ -0,0 +1,60 @@
+package com.higor.listadetarefas.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "listaTarefas")
+public class Tarefas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tarefa;
+    private String descricao;
+    private boolean realizada;
+    private int prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(String tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
