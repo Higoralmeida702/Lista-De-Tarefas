e2e2abee3d40a3c112784ea8915e8540400f7a21
Higoralmeida702 <153138268+Higoralmeida702@users.noreply.github.com>
2024-04-18 17:33:36 -0300

    feat: Adiciona @CrossOrigin(origins = "*") ao TarefasController
    Adiciona a anotação @CrossOrigin(origins = "*") ao TarefasController para permitir solicitações de origens diferentes em todos os endpoints do controlador.

--- lista-de-tarefas/src/main/java/com/higor/listadetarefas/Controllers/TarefasController.java
+++ lista-de-tarefas/src/main/java/com/higor/listadetarefas/Controllers/TarefasController.java
@@ -2,6 +2,7 @@ package com.higor.listadetarefas.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.higor.listadetarefas.Entity.Tarefas;
 import com.higor.listadetarefas.Services.TarefasService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TarefasController {
 
     @Autowired
