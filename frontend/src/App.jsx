f22d655d7c7e10c5e3c92af75b289bdc7612dc51
Higoralmeida702 <153138268+Higoralmeida702@users.noreply.github.com>
2024-04-18 21:00:44 -0300

    feat: Adiciona componente App para busca de tarefas
    Este commit adiciona o componente responsável por buscar as tarefas da API e armazená-las no estado local.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from "react"
+
 function App() {
 
+  const [tarefas, setTarefas] = useState ([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+    .then(tarefas => tarefas.json())
+    .then(tarefas_convertidas => setTarefas(tarefas))
+  },[]);
+
+  
   return (
     <div>
       
